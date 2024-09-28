8afdc7bc28ffc2647ad47466520599e5ffd9115a
Andre Arruda <andrebass27@gmail.com>
2024-09-28 17:53:06 -0300

    Creating the repository

---
+++ DevFreela.API/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        // GET api/users
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return new OkObjectResult("UsersController!");
+        }
+
+        // POST api/users
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+
+        // PUT api/users/1234/profile-picture
+        [HttpPut("{id}/profile-picture")]
+        public IActionResult PostProfilePicture(IFormFile file)
+        {
+            var description = $"File: {file.FileName}, Size: {file.Length}";
+
+            // processar a imagem
+
+            return Ok(description);
+        }
+    }
+}
