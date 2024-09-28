5a5c22ed98aa0c921a8c3d60b349744851233c94
Andre Arruda <andrebass27@gmail.com>
2024-09-28 20:06:55 -0300

    Updating User.

--- DevFreela.API/Entities/UserSkill.cs
+++ DevFreela.API/Entities/UserSkill.cs
@@ -2,7 +2,7 @@
 {
     public class UserSkill : BaseEntity
     {
-        public UserSkill(int idUser, int idSkill)
+        public UserSkill(int idUser, int idSkill) : base()
         {
             IdUser = idUser;
             IdSkill = idSkill;
