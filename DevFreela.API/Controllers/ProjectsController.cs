edd9920fda49b2a9ef6f45699fad245f5e0024fe
Andre Arruda <andrebass27@gmail.com>
2024-10-24 23:45:10 -0300

    Implementing pagination

--- DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -21,7 +21,7 @@ namespace DevFreela.API.Controllers
 
         // GET api/projects?search=crm
         [HttpGet]
-        public IActionResult Get(string search = "")
+        public IActionResult Get(string search = "", int page = 0, int size = 3)
         {
             var projects = _context.Projects
                 .Include(p => p.Client)
@@ -29,6 +29,8 @@ namespace DevFreela.API.Controllers
                 .Include(p => p.Comments)
                 .Where(p => !p.IsDeleted
                     && (search == "" || p.Title.Contains(search) || p.Description.Contains(search)))
+                .Skip(page * size)
+                .Take(size)
                 .ToList();
 
             var model = projects.Select(ProjectItemViewModel.FromEntity).ToList();
